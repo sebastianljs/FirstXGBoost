82010c44b5973c6c3580df576f5a5612b85d7d74
Sebastian Lin <sebastianljs@gmail.com>
2017-01-08 19:26:00 -0500

    Created model for breast cancer dataset

--- breast_cancer_XGBoost.py
+++ breast_cancer_XGBoost.py
@@ -1,34 +1,57 @@
+# binary classification, breast cancer dataset, label and one hot encoded
+from numpy import column_stack
 from pandas import read_csv
-from sklearn import model_selection
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
-from xgboost import XGBClassifier
+from sklearn.preprocessing import OneHotEncoder
 
-iris = read_csv('iris.csv', header = None).values
+# load data and drop missing values
 
-features = iris[:,0:-1]
-target = iris[:,-1]
+data = read_csv('breast-cancer.csv', header=None)
+data = data.dropna()
+data = data.values
 
-# Encode iris class strings as integers
+# split data into X and y
+
+features = data[:, 0:9]
+target = data[:, 9]
+
+# Encode string input values as integers
+
+columns = []
+
+for i in range(0, features.shape[1]):
+    label_encoder = LabelEncoder()
+    feature = label_encoder.fit_transform(features[:, i])
+    # Reshape feature into one column
+    feature = feature.reshape(features.shape[0], 1)
+    onehot_encoder = OneHotEncoder(sparse=False)
+    feature = onehot_encoder.fit_transform(feature)
+    columns.append(feature)
+
+features_encoded = column_stack(columns)
+print("Features shape: ", features_encoded.shape)
+
+# Encode string class values as integers
 
 label_encoder = LabelEncoder()
-label_encoder = label_encoder.fit(target)
-label_encoded_target = label_encoder.transform(target)
+target_encoded = label_encoder.fit_transform(target)
 
-# Split data in training and testing sets
-
+# Split data into training and testing datasets
 seed = 7
 test_size = 0.33
-features_train, features_test, target_train, target_test \
-    = model_selection.train_test_split(features, label_encoded_target, test_size = test_size, random_state=seed)
+features_train, features_test, target_train, target_test = \
+    train_test_split(features_encoded, target_encoded, test_size=test_size, random_state=seed)
 
-# Fit model into training data
+# Fit model using training data
 
 model = XGBClassifier()
 model.fit(features_train, target_train)
 print(model)
 
-# Make predictions for testing data
+# Make predictions using testing data
 
 target_pred = model.predict(features_test)
 predictions = [round(value) for value in target_pred]
@@ -37,8 +60,6 @@ predictions = [round(value) for value in target_pred]
 
 accuracy = accuracy_score(target_test, predictions)
 print("{:.2%}".format(accuracy))
-
-
 
 
 
