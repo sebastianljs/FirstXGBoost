16bb02d169a406559059dfbc14eb88453ca483c6
Sebastian Lin <sebastianljs@gmail.com>
2017-01-09 08:25:41 -0500

    Changed format of py file output

--- breast_cancer_XGBoost.py
+++ breast_cancer_XGBoost.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
 
-# In[1]:
-
 # binary classification, breast cancer dataset, label and one hot encoded
 from numpy import column_stack
 from pandas import read_csv
@@ -13,8 +11,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 
-# In[2]:
-
 # load data and drop missing values
 
 data = read_csv('breast-cancer.csv', header=None)
@@ -22,15 +18,11 @@ data = data.dropna()
 data = data.values
 
 
-# In[3]:
-
 # split data into X and y
 
 features = data[:, 0:9]
 target = data[:, 9]
 
-
-# In[4]:
 
 # Encode string input values as integers
 
@@ -48,15 +40,11 @@ features_encoded = column_stack(columns)
 print("Features shape: ", features_encoded.shape)
 
 
-# In[5]:
-
 # Encode string class values as integers
 
 label_encoder = LabelEncoder()
 target_encoded = label_encoder.fit_transform(target)
 
-
-# In[6]:
 
 # Split data into training and testing datasets
 
@@ -65,28 +53,18 @@ test_size = 0.33
 features_train, features_test, target_train, target_test =     train_test_split(features_encoded, target_encoded, test_size=test_size, random_state=seed)
 
 
-# In[7]:
-
 # Fit model using training data
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
 model = XGBClassifier()
-model2 = SVC()
-model3 = GaussianNB()
 model.fit(features_train, target_train)
 print(model)
 
-
-# In[8]:
 
 # Make predictions using testing data
 
 target_pred = model.predict(features_test)
 predictions = [round(value) for value in target_pred]
 
-
-# In[9]:
 
 # Evaluate predictions
 
